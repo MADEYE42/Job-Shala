ab15218fbd8c76aa64166b222776233a7d76c211
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-06-28 00:58:24 +0530

    NavBar done

--- jobsearch/src/App.jsx
+++ jobsearch/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import Search from './Components/SearchBar/Search'
+import Jobs from './Components/JobsDiv/Jobs'
+import Values from './Components/Values/Values'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='w-[85%] m-auto bg-white'>
+      <NavBar/>
+      <Search />
+      <Jobs />
+      <Values />
+      <Footer />
+    </div>
   )
 }
 
---
+++ jobsearch/src/Components/SearchBar/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className="seacrhDiv grid gp-10 bg-grey rounded-[10px] p-[3rem]">
+        <form action="">
+            <div className="firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700 ">
+                
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Search
