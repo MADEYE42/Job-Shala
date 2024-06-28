495a2368876234d2063ac01a7dab6665187db7d5
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-06-28 12:33:42 +0530

    Update NavBar.jsx

--- jobsearch/src/Components/NavBar/NavBar.jsx
+++ jobsearch/src/Components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className='navBar flex justify-between items-center p-[3rem]'>
         <div className="logoDiv">
-            <h1 className="logo text-[25px] text-blueColor"><strong>Job</strong>Search</h1>
+            <h1 className="logo text-[25px] text-blueColor"><strong>Job</strong>Shala</h1>
         </div>
         <div className="menu flex gap-8">
             <li className="menuList text-[#6f6f6f] hover:text-blueColor">Jobs</li>
