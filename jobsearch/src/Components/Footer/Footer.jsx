718cb3ea3585d4b5a9bea049dbfdaef2a8bde56f
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-07-21 02:51:46 +0530

    Update Footer.jsx

--- jobsearch/src/Components/Footer/Footer.jsx
+++ jobsearch/src/Components/Footer/Footer.jsx
@@ -1,47 +1,45 @@
 import React from 'react';
 import { AiFillInstagram } from "react-icons/ai";
-import { FaFacebook } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className=' w-full footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-8 grid grid-cols-5 items-start m-auto items-center justify-center'>
-      <div className="">
-      <div className="logoDiv">
-            <h1 className="logo text-[25px] text-white pb-[1.5rem]"><strong>Job</strong>Shala</h1>
+    <div className='w-full footer p-4 sm:p-8 lg:p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 items-start m-auto items-center justify-center'>
+      <div className="mb-4 lg:mb-0">
+        <div className="logoDiv">
+          <h1 className="logo text-[25px] text-white pb-[1.5rem]"><strong>Job</strong>Shala</h1>
         </div>
-        <p className="text-white pb-[13px] opacity-70 leading-7 ">
+        <p className="text-white pb-[13px] opacity-70 leading-7">
           We always make our seekers and companies find the best jobs and find the best candidates.
         </p>
       </div>
-      <div className="grid">
+      <div className="grid mb-4 lg:mb-0">
         <span className="divTitle text-[18px] font-semibold pb-[1.5rem] text-white">
-            Company
+          Company
         </span>
-        <div className="grid gap-3 ">
+        <div className="grid gap-3">
           <li className="text-white opacity-[.7] hover:opacity-[1]">About Us</li>
           <li className="text-white opacity-[.7] hover:opacity-[1]">Features</li>
           <li className="text-white opacity-[.7] hover:opacity-[1]">News</li>
           <li className="text-white opacity-[.7] hover:opacity-[1]">FAQ</li>
         </div>
       </div>
-      <div className="grid">
+      <div className="grid mb-4 lg:mb-0">
         <span className="divTitle text-[18px] font-semibold pb-[1.5rem] text-white">
-            Resources
+          Resources
         </span>
-        <div className="grid gap-3 ">
+        <div className="grid gap-3">
           <li className="text-white opacity-[.7] hover:opacity-[1]">Account</li>
           <li className="text-white opacity-[.7] hover:opacity-[1]">Support Center</li>
           <li className="text-white opacity-[.7] hover:opacity-[1]">Feedback</li>
           <li className="text-white opacity-[.7] hover:opacity-[1]">Contact Us</li>
         </div>
       </div>
-      <div className="grid">
+      <div className="grid mb-4 lg:mb-0">
         <span className="divTitle text-[18px] font-semibold pb-[1.5rem] text-white">
-            Support
+          Support
         </span>
-        <div className="grid gap-3 ">
+        <div className="grid gap-3">
           <li className="text-white opacity-[.7] hover:opacity-[1]">Events</li>
           <li className="text-white opacity-[.7] hover:opacity-[1]">Promo</li>
           <li className="text-white opacity-[.7] hover:opacity-[1]">Req Demo</li>
@@ -50,22 +48,22 @@ const Footer = () => {
       </div>
       <div className="grid">
         <span className="divTitle text-[18px] font-semibold pb-[1.5rem] text-white">
-            Support
+          Contact Us
         </span>
-        <div >
+        <div>
           <small className='text-[14px] text-white'>
             gmadye13@gmail.com
           </small>
-          <div className="icon flex gap-4 py-[1rem] ">
-            <AiFillInstagram className='bg-white text-blueColor icon  w-[35px] h-[35px]'/>
-            <FaFacebook className='bg-white text-blueColor icon w-[35px] h-[35px] rounded-full'/>
-            <FaGithub className='bg-white text-blueColor icon w-[35px] h-[35px] rounded-full'/>
-            <FaLinkedin className='bg-white text-blueColor icon w-[35px] h-[35px] '/>
+          <div className="icon flex gap-4 py-[1rem]">
+            <AiFillInstagram className='bg-white text-blueColor icon w-[35px] h-[35px]' />
+            <FaFacebook className='bg-white text-blueColor icon w-[35px] h-[35px] rounded-full' />
+            <FaGithub className='bg-white text-blueColor icon w-[35px] h-[35px] rounded-full' />
+            <FaLinkedin className='bg-white text-blueColor icon w-[35px] h-[35px]' />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
