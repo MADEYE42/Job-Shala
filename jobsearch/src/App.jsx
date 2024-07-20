834e0c90ac1f1948c6fd4ae253bc58e8193e525b
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-07-21 02:56:06 +0530

    everthing is mobile responsive now

--- jobsearch/src/App.jsx
+++ jobsearch/src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import NavBar from './Components/NavBar/NavBar'
-import Search from './Components/SearchBar/Search'
-import Jobs from './Components/JobsDiv/Jobs'
-import Values from './Components/Values/Values'
-import Footer from './Components/Footer/Footer'
+import React from 'react';
+import NavBar from './Components/NavBar/NavBar';
+import Search from './Components/SearchBar/Search';
+import Jobs from './Components/JobsDiv/Jobs';
+import Values from './Components/Values/Values';
+import Footer from './Components/Footer/Footer';
 
 const App = () => {
   return (
-    <div className='w-[85%] m-auto bg-white'>
-      <NavBar/>
-      <Search />
-      <Jobs />
-      <Values />
+    <div className='w-full sm:w-[85%] m-auto bg-white'>
+      <NavBar />
+      <div className="p-4 sm:p-8">
+        <Search />
+        <Jobs />
+        <Values />
+      </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- jobsearch/src/Components/NavBar/NavBar.jsx
+++ jobsearch/src/Components/NavBar/NavBar.jsx
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const NavBar = () => {
-  return (
-    <div className='navBar flex justify-between items-center p-[3rem]'>
-        <div className="logoDiv">
-            <h1 className="logo text-[25px] text-blueColor"><strong>Job</strong>Shala</h1>
-        </div>
-        <div className="menu flex gap-8">
-            <li className="menuList text-[#6f6f6f] hover:text-blueColor">Jobs</li>
-            <li className="menuList text-[#6f6f6f] hover:text-blueColor">Companies</li>
-            <li className="menuList text-[#6f6f6f] hover:text-blueColor">About</li>
-            <li className="menuList text-[#6f6f6f] hover:text-blueColor">Contact</li>
-            <li className="menuList text-[#6f6f6f] hover:text-blueColor">Blog</li>
-            <li className="menuList text-[#6f6f6f] hover:text-blueColor">Login</li>
-            <li className="menuList text-[#6f6f6f] hover:text-blueColor">Register</li>
-        </div>
-    </div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default NavBar
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className='navBar flex justify-between items-center p-4 sm:p-8 bg-white shadow-lg'>
+      <div className="logoDiv">
+        <h1 className="logo text-[25px] text-blueColor"><strong>Job</strong>Shala</h1>
+      </div>
+      <div className="menu hidden sm:flex gap-8">
+        <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">Jobs</li>
+        <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">Companies</li>
+        <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">About</li>
+        <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">Contact</li>
+        <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">Blog</li>
+        <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">Login</li>
+        <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">Register</li>
+      </div>
+      <div className="hamburgerMenu sm:hidden cursor-pointer" onClick={toggleMenu}>
+        {isOpen ? <FaTimes className="text-[25px] text-blueColor" /> : <FaBars className="text-[25px] text-blueColor" />}
+      </div>
+      {isOpen && (
+        <div className="mobileMenu absolute top-[70px] left-0 w-full bg-white shadow-lg flex flex-col items-center gap-8 py-8 sm:hidden">
+          <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">Jobs</li>
+          <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">Companies</li>
+          <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">About</li>
+          <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">Contact</li>
+          <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">Blog</li>
+          <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">Login</li>
+          <li className="menuList list-none text-[#6f6f6f] hover:text-blueColor">Register</li>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
